a2f2b43368fc8ce9a6af04041b8bbb598a3472e9
Tj Vanderpoel <tj@rubyists.com>
2011-02-27 14:36:36 -0600

    removed debugging

--- controller/cdr.rb
+++ controller/cdr.rb
@@ -1,7 +1,6 @@
 class CdrController < Controller
   def post
     uuid, xml, leg = request['uuid', 'cdr', 'leg']
-    Ramaze::Log.info xml[0, 9]
     xml = CGI.unescape(xml) if xml[0, 9] == '%3C%3Fxml'
     call = TinyCdr::Call.create_from_xml(uuid, leg, xml)
     Ramaze::Log.info "New call from #{call.username || call.caller_id_number} to #{call.destination_number}"
