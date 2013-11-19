4e7bce8412a9202f3d7b93eb6973329663827b5a
TJ Vanderpoel <tj@rubyists.com>
2013-11-19 12:20:55 -0600

    add options

--- options.rb
+++ options.rb
@@ -10,5 +10,11 @@ module TinyCdr
 
     o "Log Level (DEBUG, INFO, WARN, ERROR, CRIT)", :log_level,
       ENV["TinyCdr_LogLevel"] || "INFO"
+
+    o "Main Recording Path", :base_record_path, ENV["TinyCdr_BaseRecordPath"] || "/var/spool/freeswitch/recordings"
+
+    o "Archive Recording Path", :archive_record_path, ENV["TinyCdr_ArchiveRecordPath"] || "/mnt/recordings"
+
+    o "Mounted Recording Path", :mounted_record_path, ENV["TinyCdr_MountedRecordPath"] || "/mnt/recordings"
   end
 end
