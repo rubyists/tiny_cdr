06cce5c469694746206d28fcae4c9b8d7d7c17aa
TJ Vanderpoel <tj@rubyists.com>
2013-11-20 08:50:48 -0600

    modify tabletools buttons

--- public/js/tiny_cdr.js
+++ public/js/tiny_cdr.js
@@ -2,7 +2,30 @@ $(document).ready(function() {
   $('#user_calls').dataTable({
     "sDom": 'T<"clear">lfrtip',
     "oTableTools": {
-      "sSwfPath": "/swf/copy_csv_xls_pdf.swf"
+      "sSwfPath": "/swf/copy_csv_xls_pdf.swf",
+      "aButtons": [
+                "copy",
+                {
+                    "sExtends": "pdf",
+                    "sButtonText": "PDF - Reporting columns",
+                    "mColumns": [ 0, 1, 2, 3, 4 ]
+                },
+                {
+                    "sExtends": "pdf",
+                    "sButtonText": "PDF - All",
+                    "mColumns": "visible"
+                },
+                {
+                    "sExtends": "print",
+                    "sButtonText": "Print",
+                    "mColumns": [ 0, 1, 2, 3, 4 ]
+                },
+                {
+                    "sExtends": "csv",
+                    "sButtonText": "CSV",
+                    "mColumns": [ 0, 1, 2, 3, 4 ]
+                }
+            ]
     }
   });
 });
